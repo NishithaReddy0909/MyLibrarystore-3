211016503c4ac16c28d50dffff7bb7f61f025515
NishithaReddy0909 <nishithareddynalla0909@gmail.com>
2020-09-28 15:11:26 +0530

    Created Delete Action to CustomerController

--- MyLibrarystore/Controllers/CustomerController.cs
+++ MyLibrarystore/Controllers/CustomerController.cs
@@ -12,5 +12,9 @@ namespace MyLibrarystore.Controllers
         {
             return View();
         }
+        public IActionResult Delete()
+        {
+            return View();
+        }
     }
 }
